546d15d9ce8153ab0629cf92cb011523e31fdbec
Ntim Yeboah <lylegendary@gmail.com>
2025-03-30 13:15:32 +0000

    Add a test guage

--- app/Providers/PrometheusServiceProvider.php
+++ app/Providers/PrometheusServiceProvider.php
@@ -20,7 +20,7 @@ class PrometheusServiceProvider extends ServiceProvider
          * Here you can register all the exporters that you
          * want to export to prometheus
          */
-        Prometheus::addGauge('My gauge')
+        Prometheus::addGauge('app test guage')
             ->value(function() {
                 return 123.45;
             });
